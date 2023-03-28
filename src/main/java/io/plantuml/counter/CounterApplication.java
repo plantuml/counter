19e330e7c8f9c26886f839d8da9c57d0c6bc8b2d
Arnaud Roques <plantuml@gmail.com>
2023-03-28 22:38:52 +0200

    Feat: change context to /counter

--- src/main/java/io/plantuml/counter/CounterApplication.java
+++ src/main/java/io/plantuml/counter/CounterApplication.java
@@ -3,7 +3,10 @@ package io.plantuml.counter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CounterApplication extends SpringBootServletInitializer  {
@@ -15,6 +18,11 @@ public class CounterApplication extends SpringBootServletInitializer  {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CounterApplication.class);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath("/counter");
 	}
 
 }
